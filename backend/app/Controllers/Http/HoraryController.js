e95e9916fb464b728288c22370791fc5bb4d799d
souzar1999 <45098010+souzar1999@users.noreply.github.com>
2019-12-10 12:30:42 -0300

    Retorno simples horário backend

--- backend/app/Controllers/Http/HoraryController.js
+++ backend/app/Controllers/Http/HoraryController.js
@@ -3,50 +3,43 @@
 const Horary = use('App/Models/Horary')
 
 class HoraryController {
-  async index ({ request, response, view }) {
-    const horary = Horary.query()
-      .with('schedulings')
-      .fetch()
+  async index({ request, response, view }) {
+    const horary = Horary.query().fetch()
 
     return horary
   }
 
-  async show ({ params, request, response, view }) {
+  async show({ params, request, response, view }) {
     const horary = await Horary.query()
-      .where('id', params.id)   
-      .with('schedulings')
+      .where('id', params.id)
       .fetch()
 
-    return horary;
-  }
-
-  async store ({ request, response }) {
-    const data = request.only([
-      'time'
-    ])
-
-    const horary = await Horary.create(data);
-
-    return horary;
-  }
-
-  async update ({ params, request, response }) {
-    const horary = await Horary.findOrFail(params.id);
-    const data = request.only([
-      'time'
-    ])
-    
-    horary.merge(data);
-
-    await horary.save();
-    
     return horary
   }
 
-  async destroy ({ params, request, response }) {
+  async store({ request, response }) {
+    const data = request.only(['time'])
+
+    const horary = await Horary.create(data)
+
+    return horary
+  }
+
+  async update({ params, request, response }) {
     const horary = await Horary.findOrFail(params.id)
-    
-    await horary.delete();
+    const data = request.only(['time'])
+
+    horary.merge(data)
+
+    await horary.save()
+
+    return horary
+  }
+
+  async destroy({ params, request, response }) {
+    const horary = await Horary.findOrFail(params.id)
+
+    await horary.delete()
   }
 }
 
