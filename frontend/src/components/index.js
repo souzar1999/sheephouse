90b8ebed90ad2a4c0437b752923429203f851c2a
Vinicius Dell Osbel de Freitas <viniciusdof@gmail.com>
2020-02-29 14:35:57 -0300

    remove uuid package and fix GlobalUploader

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -33,7 +33,7 @@ import AdminReports from "./Admin/Reports/Reports";
 import GlobalScheduling from "./Global/Scheduling/Scheduling";
 import GlobalRescheduling from "./Global/Scheduling/Rescheduling";
 import GlobalFileManager from "./Global/FileManager/FileManager";
-//import GlobalFileUploader from "./Global/FileManager/FileUploader";
+import GlobalFileUploader from "./Global/FileManager/FileUploader";
 
 export {
   SidebarLayout,
